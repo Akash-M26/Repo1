9e95d92b9ffe0e185c42e635e440d029b02c68d8
AKA <aka@gmail.com>
2024-10-15 20:55:34 +0530

    Adding to github

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Function declarations (prototypes)
+void biggest3();
+void factorial();
+
+int main() {
+    biggest3();   // Call to biggest3 function
+    factorial();  // Call to factorial function
+    return 0;     // Return 0 to indicate successful execution
+}
